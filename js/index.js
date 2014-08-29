2876cbff80c4d1ce00f98b7aa98259b7edd04b92
Stephen Hewitt <stephen@intrinsitech.net>
2014-08-29 10:19:04 -0700

    new ui

--- js/index.js
+++ js/index.js
@@ -27,7 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
-        $(".app").on("click", function () {
+        $("#ToFieldConnect").on("click", function () {
             window.location = "http://192.168.0.11:8000/public/gap.htm";
         });
     },
